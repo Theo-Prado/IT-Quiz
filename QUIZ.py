351a7c9f5e8bbf30122ad35667ff400bb84b0c59
Theo <theo.silva.prado@gmail.com>
2026-05-02 18:36:44 -0300

    Translate quiz to English

--- QUIZ.py
+++ QUIZ.py
@@ -1,64 +1,64 @@
-print("==========Quiz=de=TI==========")
+print("==========IT Quiz==========")
 
 print()
 
-pontos = 0
+points = 0
 
-p1 = input("Quem foi a primeira programadora do mundo?")
+p1 = input("Who was the first programmer in the world?")
 if p1.lower() == "ada lovelace" or p1.lower() == "ada" or p1.lower() == "lovelace":
-  pontos += 1
-  print("Correto! Pontos:",pontos,"/ 6")
+  points += 1
+  print("Correct! Points:",points,"/ 6")
 else:
-  print("Errado! A primeira programadora do mundo foi Ada Lovelace.")
+  print("Wrong! The first programmer in the world was Ada Lovelace.")
 
 print()
 
-p2 = input("Quem inventou a linguagem de programação Python?")
+p2 = input("Who invented the Python programming language?")
 if p2.lower() == "guido" or p2.lower() == "guido van" or p2.lower() == "guido van rossum":
-  pontos += 1
-  print("Correto! Pontos:",pontos,"/ 6")
+  points += 1
+  print("Correct! Points:",points,"/ 6")
 else:
-  print("Errado! Quem inventou a linguagem de programação Python foi Guido van Rossum.")
+  print("Wrong! Who invented the Python programming language was Guido van Rossum.")
 
 print()
 
-p3 = input("Qual tipo de sistema de software a TOTVS desenvolve?")
-if p3.lower() == "erp" or p3.lower() == "enterprise resource planning" or p3.lower() == "planejamento de recursos empresariais":
-  pontos += 1
-  print("Correto! Pontos:",pontos,"/ 6")
+p3 = input("What type of software system does TOTVS develop?")
+if p3.lower() == "erp" or p3.lower() == "enterprise resource planning":
+  points += 1
+  print("Correct! Points:",points,"/ 6")
 else:
-  print("Errado! A TOTVS desenvolve sistema de ERP (Enterprise Resource Planning).")
+  print("Wrong! TOTVS develops ERP system (Enterprise Resource Planning).")
 
 print()
 
-p4 = input("Quem é considerado o pai da Inteligência Artificial?")
-if p4.lower() == "Alan" or p4.lower() == "Turing" or p4.lower() == "Alan Turing":
-  pontos += 1
-  print("Correto! Pontos:",pontos,"/ 6")
+p4 = input("Who is considered the father of Artificial Intelligence?")
+if p4.lower() == "alan" or p4.lower() == "turing" or p4.lower() == "alan turing":
+  points += 1
+  print("Correct! Points:",points,"/ 6")
 else:
-  print("Errado! O pai da Inteligência Artificial é Alan Turing.")
+  print("Wrong! The father of Artificial Intelligence is Alan Turing.")
 
 print()
 
-p5 = input("Qual é o nome dado ao conjunto de instruções, dados ou programas lógicos usados para operar computadores e dispositivos?")
+p5 = input("What is the name given to the set of instructions, data or logical programs used to operate computers and devices?")
 if p5.lower() == "software":
-  pontos += 1
-  print("Correto! Pontos:",pontos,"/ 6")
+  points += 1
+  print("Correct! Points:",points,"/ 6")
 else:
-  print("Errado! É o software.")
+  print("Wrong! It is software.")
 
 print()
 
-p6 = input("O que é a parte física e tangível de computadores e dispositivos eletrônicos, incluindo componentes internos (processador, memória, placa-mãe) e externos (mouse, teclado, monitor)")
+p6 = input("What is the physical and tangible part of computers and electronic devices, including internal components (processor, memory, motherboard) and external (mouse, keyboard, monitor)?")
 if p6.lower() == "hardware":
-  pontos += 1
-  print("Correto! Pontos:",pontos,"/ 6")
+  points += 1
+  print("Correct! Points:",points,"/ 6")
 else:
-  print("Errado! É o hardware.")
+  print("Wrong! It is hardware.")
 
 print()
 
-if pontos == 3:
-  print("Parabéns, você ganhou! Acertou todas as questões!")
+if points == 6:
+  print("Congratulations, you won! You got all the questions right!")
 else:
-  print("Você perdeu! Acertou",pontos,"/ 6!")
+  print("You lost! You got",points,"/ 6 correct!")
