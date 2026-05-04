3302b423601a806e4d5d5df93a9ead2ac9db50be
Theo <theo.silva.prado@gmail.com>
2026-05-04 19:15:41 -0300

    Update quiz to include ten questions instead of six

--- QUIZ.py
+++ QUIZ.py
@@ -4,7 +4,7 @@ print()
 name = input("Enter your name:")
 print()
 print(f"Welcome, {name}!")
-print("This quiz has six questions about the world of Information Technology.")
+print("This quiz has ten questions about the world of Information Technology.")
 print("Can you get them all right?")
 print(f"Play now and find out,{name}!")
 print()
@@ -13,7 +13,7 @@ points = 0
 p1 = input("Who was the first programmer in the world?")
 if p1.lower() == "ada lovelace" or p1.lower() == "ada" or p1.lower() == "lovelace":
   points += 1
-  print("Correct! Points:",points,"/ 6")
+  print("Correct! Points:",points,"/ 10")
 else:
   print("Wrong! The first programmer in the world was Ada Lovelace.")
 
@@ -22,7 +22,7 @@ print()
 p2 = input("Who invented the Python programming language?")
 if p2.lower() == "guido" or p2.lower() == "guido van" or p2.lower() == "guido van rossum":
   points += 1
-  print("Correct! Points:",points,"/ 6")
+  print("Correct! Points:",points,"/ 10")
 else:
   print("Wrong! Who invented the Python programming language was Guido van Rossum.")
 
@@ -31,7 +31,7 @@ print()
 p3 = input("What type of software system does TOTVS develop?")
 if p3.lower() == "erp" or p3.lower() == "enterprise resource planning":
   points += 1
-  print("Correct! Points:",points,"/ 6")
+  print("Correct! Points:",points,"/ 10")
 else:
   print("Wrong! TOTVS develops ERP system (Enterprise Resource Planning).")
 
@@ -40,7 +40,7 @@ print()
 p4 = input("Who is considered the father of Artificial Intelligence?")
 if p4.lower() == "alan" or p4.lower() == "turing" or p4.lower() == "alan turing":
   points += 1
-  print("Correct! Points:",points,"/ 6")
+  print("Correct! Points:",points,"/ 10")
 else:
   print("Wrong! The father of Artificial Intelligence is Alan Turing.")
 
@@ -49,7 +49,7 @@ print()
 p5 = input("What is the name given to the set of instructions, data or logical programs used to operate computers and devices?")
 if p5.lower() == "software":
   points += 1
-  print("Correct! Points:",points,"/ 6")
+  print("Correct! Points:",points,"/ 10")
 else:
   print("Wrong! It is software.")
 
@@ -58,18 +58,27 @@ print()
 p6 = input("What is the physical and tangible part of computers and electronic devices, including internal components (processor, memory, motherboard) and external (mouse, keyboard, monitor)?")
 if p6.lower() == "hardware":
   points += 1
-  print("Correct! Points:",points,"/ 6")
+  print("Correct! Points:",points,"/ 10")
 else:
   print("Wrong! It is hardware.")
 
 print()
 
-if points == 6:
-  print("Congratulations, you won! You got all the questions right!")
-elif points >= 4:
-  print("Good! You got",points,"/ 6 correct!")
-elif points == 3:
-  print("Median score. You got",points,"/ 6 correct!")
+p7 = input("What is the name of an accumulated list of unfinished tasks, requirements, or work items that need to be addressed, often prioritized by importance?")
+if p7.lower() == "backlog"
+  points += 1
+  print("Correct! Points:",points,"/ 10")
 else:
-  print("You lost! You got",points,"/ 6 correct!")
+  print("Wrong! It is backlog.")
+
+print()
+
+if points == 10:
+  print("Congratulations, you won! You got all the questions right!")
+elif points >= 6:
+  print("Good! You got",points,"/ 10 correct!")
+elif points == 5:
+  print("Median score. You got",points,"/ 10 correct!")
+else:
+  print("You lost! You got",points,"/ 10 correct!")
 print(f"Thank you for playing, {name}!
