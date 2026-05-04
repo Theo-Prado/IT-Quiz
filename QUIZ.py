57a44842bf9e214bc39c41eff32a1a1172370523
Theo <theo.silva.prado@gmail.com>
2026-05-04 19:31:01 -0300

    Fix syntax for if statements in QUIZ.py

--- QUIZ.py
+++ QUIZ.py
@@ -65,7 +65,7 @@ else:
 print()
 
 p7 = input("What is the name of an accumulated list of unfinished tasks, requirements, or work items that need to be addressed, often prioritized by importance?")
-if p7.lower() == "backlog"
+if p7.lower() == "backlog":
   points += 1
   print("Correct! Points:",points,"/ 10")
 else:
@@ -74,7 +74,7 @@ else:
 print()
 
 p8 = input("What is the process of reverting a system, database, or software application to a previous, stable state to undo errors, faulty updates, or undesirable changes?")
-if p8.lower() == "rollback"
+if p8.lower() == "rollback":
   points += 1
   print("Correct! Points:",points,"/ 10")
 else:
@@ -83,14 +83,14 @@ else:
 print()
 
 p9 = input("What is a distinctive attribute, aspect, or functionality of a product, system, or service that adds value, improves usability, or makes it unique?")
-if p9.lower() == "feature"
+if p9.lower() == "feature":
   points += 1
   print("Correct! Points:",points,"/ 10")
 else:
   print("Wrong! It is feature.")
 
 p10 = input("what is the most recommended language for automation?")
-if p10.lower() == "python"
+if p10.lower() == "python":
   points += 1
   print("Correct! Points:",points,"/ 10")
 else:
